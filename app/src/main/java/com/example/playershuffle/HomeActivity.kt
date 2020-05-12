0713c082c6412115bc45273e874d8bd522f176ef
Taren <taren.lewis@gmail.com>
2020-05-12 17:01:17 -0500

    Testing collab, pushing broken changes

--- app/src/main/java/com/example/playershuffle/HomeActivity.kt
+++ app/src/main/java/com/example/playershuffle/HomeActivity.kt
@@ -3,7 +3,7 @@ package com.example.playershuffle
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
