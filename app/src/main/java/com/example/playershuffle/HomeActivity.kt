063e62b84aa1b5b1ff994d3675e253b1a505b64e
Taren <taren.lewis@gmail.com>
2020-05-12 17:01:51 -0500

    Testing collab, pushing broken changes

--- app/src/main/java/com/example/playershuffle/HomeActivity.kt
+++ app/src/main/java/com/example/playershuffle/HomeActivity.kt
@@ -7,6 +7,6 @@ class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
     }
 }
